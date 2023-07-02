74a2536022be577f2e11a56029878b85387ae1be
Andrii Naida <work.andrew.naida@gmail.com>
2023-07-02 20:40:50 +0200

    minor changes

--- src/app/libs/helpers.ts
+++ src/app/libs/helpers.ts
@@ -1,4 +1,4 @@
-import { Price } from "../../types";
+import { Price } from "../types";
 
 export const getURL = () => {
     let url =
--- src/app/libs/supabaseAdmin.ts
+++ src/app/libs/supabaseAdmin.ts
@@ -1,7 +1,7 @@
 import Stripe from "stripe";
 import { createClient } from "@supabase/supabase-js";
-import { Database } from "../../types_db";
-import { Price, Product } from "../../types";
+import { Database } from "../types_db";
+import { Price, Product } from "../types";
 import { stripe } from "./stripe";
 import { toDateTime } from "./helpers";
 
