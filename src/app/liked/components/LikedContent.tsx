f48e757958d7e5c04d43f4c6acf6c324343ef7e9
Andrii Naida <work.andrew.naida@gmail.com>
2023-07-04 16:58:13 +0200

    styles fix

--- src/app/liked/components/LikedContent.tsx
+++ src/app/liked/components/LikedContent.tsx
@@ -34,7 +34,7 @@ export const LikedContent: React.FC<LikedContentProps> = ({
     }
 
     return (
-        <div>
+        <div className="flex flex-col gap-y-2 w-full p-6">
             {songs.map((song) => (
                 <div key={song.id}
                 className="flex items-center gap-x-4 w-full">
